8f2cd3e46e18968081ff3e22736ceaa3340e8d8c
Lukas Kluft <lukas.kluft@gmail.com>
2017-02-23 21:00:30 +0100

    Improved reading and writing functionality.
    * (write):
    
    Create a proper file header in Wettermast format when writing files.
    
    * (read):
    
    It is now possible to set a value for missing data. If no value is
    passed, the `DefaultValue` specified in the file header is used.
    
    * Small documentation fixes.

--- lehrex/csv.py
+++ lehrex/csv.py
@@ -2,7 +2,7 @@
 """Load CSV files stored in Wettermast format.
 
 Attributes:
-    variable_dtypes (dict): Stores the dtype related to a variable name.
+    VARIABLE_DTYPES (dict): Stores the dtype related to a variable name.
 """
 import re
 
@@ -17,7 +17,7 @@ __all__ = [
     'write',
 ]
 
-variable_dtypes = {
+VARIABLE_DTYPES = {
     'DATE': '<U10',
     'TIME': '<U5',
 }
@@ -37,6 +37,25 @@ def _get_mpl_date(dates, fmt='%d.%m.%Y %H:%M'):
     """
     return np.array([strpdate2num(fmt)(d) for d in dates])
 
+
+def _get_default_value(filename):
+    """Get default value for missing data from CSV file header.
+
+    Parameters:
+        filename (str): Path to CSV file.
+
+    Returns:
+        float or str: Default value for mising values.
+    """
+    with open(filename, "rb") as f:
+        for line in f:
+            if line.decode().startswith('$DefaultValue='):
+                default = line.decode().split('=')[1]
+                try:
+                    return int(default)
+                except:
+                    return default
+        return np.nan
 
 def _get_names(filename):
     """Get variable names from CSV file header.
@@ -73,21 +92,27 @@ def _get_skip_header(filename):
     raise Exception('No valid line found.')
 
 
-def read(filename, variables=None, delimiter=';', output=None):
+def read(filename, variables=None, delimiter=';', filling_values=None,
+         output=None, **kwargs):
     """Read CSV files.
 
     Parameters:
         filename (str): Path to CSV file.
         variables (List[str]): List of variables to extract.
         delimiter (str): The string used to separate values.
+        filling_values (float): Value to use for missing values.
+            If `None`, use the `DefaultValue` specified in file header.
         output (dict): Dictionary that is updated with read data.
+        **kwargs: Additional keyword arguments passed to `np.genfromtxt`.
 
     Returns:
         dict: Dictionary containing the data arrays.
     """
-    names = _get_names(filename)
     skip_header = _get_skip_header(filename)
-    dtype = [(n, variable_dtypes.get(n, 'f8')) for n in names.split(',')]
+    names = _get_names(filename)
+    dtype = [(n, VARIABLE_DTYPES.get(n, 'f8')) for n in names.split(',')]
+    if filling_values is None:
+        filling_values = _get_default_value(filename)
 
     # Read DATE and TIME even if they are not explicitly listed.
     if variables is not None:
@@ -103,7 +128,9 @@ def read(filename, variables=None, delimiter=';', output=None):
             skip_header=skip_header,
             dtype=dtype,
             names=names,
+            filling_values=filling_values,
             usecols=usecols,
+            **kwargs,
             )
 
     # Convert structured array to dictionary.
@@ -120,15 +147,15 @@ def read(filename, variables=None, delimiter=';', output=None):
     return output
 
 
-def read_profile(filename, var_regex=None, var_key='PROFILE', output=None):
+def read_profile(filename, var_regex=None, var_key='PROFILE', **kwargs):
     """Read scattering coefficients from CSV file.
 
     Parameters:
         filename (str): Path to CSV file.
-        output (dict): Dictionary that is updated with read data.
         var_regex (str): Python regular expression [0] matching
             the variable name of the profile.
         var_key (str): Dictionary key for extracted profile.
+        **kwargs: Additional keyword arguments passed to `read`.
 
     [0] https://docs.python.org/3.1/library/re.html
 
@@ -137,7 +164,7 @@ def read_profile(filename, var_regex=None, var_key='PROFILE', output=None):
     """
     profile_key = var_key + '_Z'
 
-    output = read(filename, output=output)
+    output = read(filename, **kwargs)
 
     p = re.compile(var_regex)
 
@@ -155,15 +182,15 @@ def read_profile(filename, var_regex=None, var_key='PROFILE', output=None):
     return output
 
 
-def read_scat(filename, var_regex='CLB_B\d{5}', var_key='CLB_MATRIX',
-              output=None):
+def read_scat(filename, var_regex='CLB_B\d{5}', var_key='CLB_MATRIX'):
     """Read scattering coefficients from CSV file.
 
     Parameters:
         filename (str): Path to CSV file.
-        output (dict): Dictionary that is updated with read data.
-        scat_name (str): Python regular expression [0] matching
-            the variable name of the scattering coefficients.
+        var_regex (str): Python regular expression [0] matching
+            the variable name of the profile.
+        var_key (str): Dictionary key for extracted profile.
+        **kwargs: Additional keyword arguments passed to `read_profile`.
 
     [0] https://docs.python.org/3.1/library/re.html
 
@@ -193,6 +220,9 @@ def write(filename, data, variables=None):
         data (dict): Dictionary containing data.
         variables (list[str]): Variables to store.
     """
+    if not ('DATE' in data and 'TIME' in data):
+        raise Exception('Need DATE and TIME to write proper files.')
+
     if variables is None:
         variables = list(sorted(data.keys()))
         # Write date and time to first columns.
@@ -201,13 +231,34 @@ def write(filename, data, variables=None):
                 variables.remove(k)
                 variables.insert(0, k)
 
-    # TODO: Create full header information.
-    header = '$Names=' + ";".join(variables)
-    data = np.vstack(data[v] for v in variables).T
+    # Never write MPLTIME. It is created when reading anyways.
+    variables.remove('MPLTIME')
+
+    # Convert dictionart entries to data matrix.
+    data_matrix = np.vstack(data[v] for v in variables).T
+
+    # Calculate some time statistics in Wettermast style.
+    # It looks messy and it is not clear, if those values are needed.
+    # But the results arer equivalent to the original files.
+    base_date = '27.03.1995 00:00:00'
+    dates = [' '.join(d) for d in zip(data['DATE'], data['TIME'])]
+    first_date = dates[0] + ':00'
+    mpl_times = _get_mpl_date([base_date, first_date], fmt='%d.%m.%Y %H:%M:%S')
+    secs_since = np.diff(mpl_times) * 24 * 3600  # time span in seconds
+    time_lag = np.round(np.mean(np.diff(_get_mpl_date(dates) * 24 * 3600)))
+
+    # Combine time properties to file header.
+    header = '#={}\n'.format(data_matrix.shape[0])
+    header += '$FirstDateTime={}\n'.format(first_date)
+    header += '$JSDBaseDateTime={}\n'.format(base_date)
+    header += '$FirstJSD={}\n'.format(int(secs_since))
+    header += '$TimeLagSec={}\n'.format(int(time_lag))
+    header += '$DefaultValue=99999\n'
+    header += '$Names=' + ";".join(variables)
 
     np.savetxt(
         filename,
-        data,
+        data_matrix,
         comments='',
         delimiter=';',
         header=header,
