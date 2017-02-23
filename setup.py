ab6fee4b6df4320e745ccf9888f7bb0385d0087f
Lukas Kluft <lukas.kluft@gmail.com>
2017-02-23 12:02:36 +0100

    Updated package version.

--- setup.py
+++ setup.py
@@ -12,15 +12,14 @@ setup(
     author='Lukas Kluft',
     author_email='lukas.kluft@gmail.com',
     url='https://github.com/lkluft/lehrex',
-    download_url='https://github.com/lkluft/lehrex/tarball/0.1',
-    version='0.1',
+    download_url='https://github.com/lkluft/lehrex/tarball/0.1.1',
+    version='0.1.1',
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
     classifiers=[
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Intended Audience :: Science/Research',
-        'Topic :: Atmospheric Science',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
