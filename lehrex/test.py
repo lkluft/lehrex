1ff494dbea9a3b64a725f636bfb4161dfaa196e6
Lukas Kluft <lukas.kluft@gmail.com>
2017-09-14 14:39:57 +0200

    Add test test.

---
+++ lehrex/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 Lukas Kluft <lukas.kluft@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+class TestClass():
+    def test_basic(self):
+        import lehrex
