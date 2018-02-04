c1cf57d5ced33b7318650e13cda901c95b4ff346
Lukas Kluft <lukas.kluft@gmail.com>
2018-02-04 17:03:53 +0100

    Adapt plotting functions to work with DataFrames.
    Remove `heatmap()`.

--- lehrex/plots/plots.py
+++ lehrex/plots/plots.py
@@ -4,12 +4,13 @@
 from matplotlib import dates
 import matplotlib.pyplot as plt
 
+from .common import get_label
+
 
 __all__ = [
     'set_date_axis',
     'timeseries',
     'timeseries2d',
-    'heatmap',
 ]
 
 
@@ -28,15 +29,15 @@ def set_date_axis(ax=None, dateformat='%d.%m.'):
     ax.grid('on', which='both', linestyle='-')
 
 
-def timeseries(time, data, xlabel='Datum', ylabel='', dateformat='%d.%m.',
+def timeseries(dataframe, key, xlabel='Datum', label=None, dateformat='%d.%m.',
                ax=None, **kwargs):
     """Create a basic timeseries plot.
 
     Parameters:
-        time (array): Date array in matplotlib time format.
-        data (array): Data array.
+        dataframe (pd.Dataframe): Dataframe.
+        key (str): Variable key.
         xlabel (str): x label.
-        ylabel (str): y label.
+        label (str): Line label.
         dateformat (str): Format string for date labels on xaxis.
         ax (AxesSubplot): Matplotlib axes.
         **kwargs: Additional keyword arguments passed to `plt.plot`.
@@ -47,104 +48,44 @@ def timeseries(time, data, xlabel='Datum', ylabel='', dateformat='%d.%m.',
     if ax is None:
         ax = plt.gca()
 
-    line, = ax.plot(time, data, **kwargs)
+    if label is None:
+        label = get_label(key, label='{name}')
+
+    line, = ax.plot(dataframe.index, dataframe[key], label=label, **kwargs)
     set_date_axis(ax, dateformat)
 
-    ax.set_xlim(time.min(), time.max())
     ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
     ax.legend()
 
     return line
 
 
-def timeseries2d(time, height, data, xlabel='Datum', ylabel='Höhe [m]',
-                 zlabel='', dateformat='%d.%m.', rasterized=True, ax=None,
-                 **kwargs):
+def timeseries2d(x, y, c, xlabel='Datum', ylabel='Höhe [m]',
+                 zlabel='', dateformat='%d.%m.', ax=None, **kwargs):
     """Create a basic 2D timeseries plot.
 
     Parameters:
-        time (array): Date array in matplotlib time format.
-        height (array): Height array.
-        data (array): Data array.
+        x, y, c (ndarray): Data arrays.
         xlabel (str): x label.
         ylabel (str): y label.
         zlabel (str): Colorbar label.
         dateformat (str): Format string for date labels on xaxis.
         ax (AxesSubplot): Matplotlib axes.
-        **kwargs: Additional keyword arguments passed to `plt.plot`.
+        **kwargs: Additional keyword arguments passed to `plt.pcolormesh`.
 
     Returns:
-        Line2D: A line.
+        matplotlib.collections.QuadMesh
     """
     if ax is None:
         ax = plt.gca()
 
-    def_kwargs = {
-        'rasterized': rasterized,
-        'cmap': 'viridis',
-        }
-
-    def_kwargs.update(kwargs)
-
-    ret = ax.pcolormesh(time, height, data, **def_kwargs)
+    ret = ax.pcolormesh(x, y, c, **kwargs)
 
     cb = ax.get_figure().colorbar(ret)
     cb.set_label(zlabel)
 
     set_date_axis(ax, dateformat)
-    ax.set_xlim(time.min(), time.max())
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-
-    return ret
-
-
-def heatmap(x, y, bins=20, zlabel='Anzahl', rasterized=True,
-            ax=None, **kwargs):
-    """Plot a heatmap of given data.
-
-    Parameters:
-        x (ndarray): x data.
-        y (ndarray): y data.
-        bins (None | int | [int, int] | array_like | [array, array]):
-
-            The bin specification:
-
-                - If int, the number of bins for the two dimensions
-                (nx=ny=bins).
-
-                - If [int, int], the number of bins in each dimension
-                (nx, ny = bins).
-
-                - If array_like, the bin edges for the two dimensions
-                (x_edges=y_edges=bins).
-
-                - If [array, array], the bin edges in each dimension
-                (x_edges, y_edges = bins).
-
-            The default value is 20.
-        zlabel (str): Colobar label.
-        ax (AxesSubplot): Axes to plot in.
-        **kwargs: Additional keyword argumens passed to `plt.hist2d`.
-
-    Returns:
-        Return values of `plt.hist2d`: counts, xedges, yedges, Image.
-    """
-    if ax is None:
-        ax = plt.gca()
-
-    def_kwargs = {
-        'bins': bins,
-        'rasterized': rasterized,
-        'cmap': plt.get_cmap('magma_r', 10),
-        }
-
-    def_kwargs.update(kwargs)
-
-    ret = ax.hist2d(x, y, **def_kwargs)
-
-    cb = ax.get_figure().colorbar(ret[3])
-    cb.set_label(zlabel)
 
     return ret
