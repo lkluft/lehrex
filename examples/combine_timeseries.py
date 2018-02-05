d6be38025d31a98810e91c79c57e6697292b7e22
Lukas Kluft <lukas.kluft@gmail.com>
2018-02-05 16:33:15 +0100

    Fix example for combining several DataFrames.

--- examples/combine_timeseries.py
+++ examples/combine_timeseries.py
@@ -5,8 +5,8 @@ import lehrex as lex
 
 # Read three radiation timeseries into one DataFrame.
 df = lex.read('data/MASTER0.txt')
-lex.read('data/MASTER1.txt', output=df)
-lex.read('data/MASTER2.txt', output=df)
+df = lex.read('data/MASTER1.txt', output=df)
+df = lex.read('data/MASTER2.txt', output=df)
 
 # Plot timeseries.
 fig, ax = plt.subplots()
