d6ad00616ad094e363c99b55acf711b53d013b21
Lukas Kluft <lukas.kluft@gmail.com>
2018-02-06 11:29:52 +0100

    Fix reading of files in RAW format.

--- lehrex/csv/raw.py
+++ lehrex/csv/raw.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """Load CSV files stored in RAW format.
 """
-import numpy as np
-from matplotlib import dates
+import re
+from warnings import warn
+from datetime import datetime
+
+import pandas as pd
 
 
 __all__ = [
@@ -10,90 +13,64 @@ __all__ = [
 ]
 
 
-def _get_names(filename, delimiter=';', comments='%'):
-    """Get variable names from CSV file in RAW format.
+def _parse_starttime(filename):
+    """Parse start time from file header and return datetime object."""
+    with open(filename, 'rt', encoding='UTF-8', errors='ignore') as f:
+        header = f.readline()
 
-    Parameters:
-        filename (str): Path to CSV file.
-        delimiter (str): The string used to separate values.
-        comments (str): The character used to indicate the start of a comment.
-
-    Returns:
-        list[str]: Variable names.
-    """
-    for line in open(filename, 'rt', encoding='UTF-8', errors='ignore'):
-        if not line.startswith(comments):
-            return line.strip().split(delimiter)
+    return re.compile('.*: (.*) UTC.*').match(header).group(1)
 
 
-def _get_mpl_start(filename):
-    with open(filename) as f:
-        return dates.datestr2num(':'.join(f.readline().split(':')[1:]))
+def _get_date_range(filename, freq, periods):
+    """Return array of datetime objects from file."""
+    start = datetime.strptime(
+        _parse_starttime(filename),
+        '%d.%m.%Y %H:%M:%S,%f'
+    )
+
+    return pd.date_range(start, freq=freq, periods=periods)
 
 
-def _decimal_converter(filename, comments='%'):
-    """Convert decimal separator from comma to period.
-
-    While doing so also ignore weirdly encoded characters.
-
-    Parameters:
-        filename (str): Path to CSV file.
-        comments (str): The character used to indicate the start of a comment.
-
-    Returns:
-        iterator: Return byte objects that can be read by `np.genfromtxt`.
-    """
-    for line in open(filename, 'rt', encoding='UTF-8', errors='ignore'):
-        if line.startswith(comments):
-            yield line.encode()
-        else:
-            yield line.replace(',', '.').encode()
-
-
-def read_raw(filename, datarate=20, delimiter=';', comments='%',
-             variables=None, output=None, **kwargs):
+def read_raw(filename, freq='50ms', decimal=',', delimiter=';', comment='%',
+             header=0, output=None, **kwargs):
     """Read file in RAW format.
 
     Parameters:
         filename (str): Path to CSV file.
-        datarate (float): Measurement rate [Hz].
+        freq (str or pd.DateOffset): Data frequency see `pd.read_csv`.
+        decimal (str): Character to recognize as decimal point.
         delimiter (str): The string used to separate values.
-        comments (str): The character used to indicate the start of a comment.
-        filling_values (float): Value to use for missing values.
-            If `None`, use the `DefaultValue` specified in file header.
-        variables (List[str]): List of variables to extract.
-        output (dict): Dictionary that is updated with read data.
-        **kwargs: Additional keyword arguments passed to `np.genfromtxt`.
+        comment (str): The character used to indicate a comment line.
+        header (int): Row number to use as the column names.
+            See `pd.read_csv` for further information.
+        output (pd.DataFrame): DataFrame to append read data to.
+        **kwargs: Additional keyword arguments passed to `pd.read_csv`.
 
     Returns:
         dict: Dictionary containing the data arrays.
     """
-    names = _get_names(filename, delimiter, comments)
-    if variables is None:
-        variables = names
-
-    data = np.genfromtxt(
-        _decimal_converter(filename, comments),
-        delimiter=';',
-        comments=comments,
-        names=names,
-        usecols=variables,
-        skip_header=2,
+    df = pd.read_csv(
+        filename,
+        comment=comment,
+        decimal=decimal,
+        delimiter=delimiter,
+        header=header,
         **kwargs,
-        )
+    )
+
+    try:
+        # Try to parse the start time from file header.
+        df['DATETIME'] = _get_date_range(
+            filename, freq=freq, periods=df.shape[0])
+    except ValueError:
+        # Do not set index, if the regex in `_get_date_range` does not match.
+        warn('Could not parse dateformat in file header. No data index set.')
+    else:
+        # Use date range as index if it is parsed correctly.
+        df = df.set_index('DATETIME')
 
     # Convert structured array to dictionary.
-    if output is None:
-        output = {var: data[var] for var in variables}
-    else:
-        for var in variables:
-            output[var] = data[var]
+    if output is not None:
+        return output.append(df)
 
-    # TODO: Parsing the actual data rate from file.
-    # Create time array using start time and data rate.
-    start = _get_mpl_start(filename)  # Start time in MPL format.
-    N = data.shape[0]  # Number of measurements.
-    dt = 1 / (24 * 60 * 60 * datarate)  # Time difference betwen measurements.
-    output['MPLTIME'] = np.arange(start, start + N * dt, dt)
-
-    return output
+    return df
